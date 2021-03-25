dbef341e00a62b8481e95d6a81077466503c2ec4
aegooby <38363513+aegooby@users.noreply.github.com>
2021-03-25 21:38:34 +0300

    Update ApplePayDomains.d.ts

--- stripe/2020-08-27/ApplePayDomains.d.ts
+++ stripe/2020-08-27/ApplePayDomains.d.ts
@@ -1,126 +1,122 @@
 // File generated from our OpenAPI spec
 
-declare module 'stripe' {
-  namespace Stripe {
-    /**
-     * The ApplePayDomain object.
-     */
-    interface ApplePayDomain {
-      /**
-       * Unique identifier for the object.
-       */
-      id: string;
+/**
+ * The ApplePayDomain object.
+ */
+interface ApplePayDomain {
+  /**
+   * Unique identifier for the object.
+   */
+  id: string;
 
-      /**
-       * String representing the object's type. Objects of the same type share the same value.
-       */
-      object: 'apple_pay_domain';
+  /**
+   * String representing the object's type. Objects of the same type share the same value.
+   */
+  object: 'apple_pay_domain';
 
-      /**
-       * Time at which the object was created. Measured in seconds since the Unix epoch.
-       */
-      created: number;
+  /**
+   * Time at which the object was created. Measured in seconds since the Unix epoch.
+   */
+  created: number;
 
-      deleted?: void;
+  deleted?: void;
 
-      domain_name: string;
+  domain_name: string;
 
-      /**
-       * Has the value `true` if the object exists in live mode or the value `false` if the object exists in test mode.
-       */
-      livemode: boolean;
-    }
+  /**
+   * Has the value `true` if the object exists in live mode or the value `false` if the object exists in test mode.
+   */
+  livemode: boolean;
+}
 
-    /**
-     * The DeletedApplePayDomain object.
-     */
-    interface DeletedApplePayDomain {
-      /**
-       * Unique identifier for the object.
-       */
-      id: string;
+/**
+ * The DeletedApplePayDomain object.
+ */
+interface DeletedApplePayDomain {
+  /**
+   * Unique identifier for the object.
+   */
+  id: string;
 
-      /**
-       * String representing the object's type. Objects of the same type share the same value.
-       */
-      object: 'apple_pay_domain';
+  /**
+   * String representing the object's type. Objects of the same type share the same value.
+   */
+  object: 'apple_pay_domain';
 
-      /**
-       * Always true for a deleted object
-       */
-      deleted: true;
-    }
+  /**
+   * Always true for a deleted object
+   */
+  deleted: true;
+}
 
-    interface ApplePayDomainCreateParams {
-      domain_name: string;
+interface ApplePayDomainCreateParams {
+  domain_name: string;
 
-      /**
-       * Specifies which fields in the response should be expanded.
-       */
-      expand?: Array<string>;
-    }
+  /**
+   * Specifies which fields in the response should be expanded.
+   */
+  expand?: Array<string>;
+}
 
-    interface ApplePayDomainRetrieveParams {
-      /**
-       * Specifies which fields in the response should be expanded.
-       */
-      expand?: Array<string>;
-    }
+interface ApplePayDomainRetrieveParams {
+  /**
+   * Specifies which fields in the response should be expanded.
+   */
+  expand?: Array<string>;
+}
 
-    interface ApplePayDomainListParams extends PaginationParams {
-      domain_name?: string;
+interface ApplePayDomainListParams extends PaginationParams {
+  domain_name?: string;
 
-      /**
-       * Specifies which fields in the response should be expanded.
-       */
-      expand?: Array<string>;
-    }
+  /**
+   * Specifies which fields in the response should be expanded.
+   */
+  expand?: Array<string>;
+}
 
-    interface ApplePayDomainDeleteParams {}
+interface ApplePayDomainDeleteParams {}
 
-    class ApplePayDomainsResource {
-      /**
-       * Create an apple pay domain.
-       */
-      create(
-        params: ApplePayDomainCreateParams,
-        options?: RequestOptions
-      ): Promise<Stripe.Response<Stripe.ApplePayDomain>>;
+class ApplePayDomainsResource {
+  /**
+   * Create an apple pay domain.
+   */
+  create(
+    params: ApplePayDomainCreateParams,
+    options?: RequestOptions
+  ): Promise<Stripe.Response<Stripe.ApplePayDomain>>;
 
-      /**
-       * Retrieve an apple pay domain.
-       */
-      retrieve(
-        id: string,
-        params?: ApplePayDomainRetrieveParams,
-        options?: RequestOptions
-      ): Promise<Stripe.Response<Stripe.ApplePayDomain>>;
-      retrieve(
-        id: string,
-        options?: RequestOptions
-      ): Promise<Stripe.Response<Stripe.ApplePayDomain>>;
+  /**
+   * Retrieve an apple pay domain.
+   */
+  retrieve(
+    id: string,
+    params?: ApplePayDomainRetrieveParams,
+    options?: RequestOptions
+  ): Promise<Stripe.Response<Stripe.ApplePayDomain>>;
+  retrieve(
+    id: string,
+    options?: RequestOptions
+  ): Promise<Stripe.Response<Stripe.ApplePayDomain>>;
 
-      /**
-       * List apple pay domains.
-       */
-      list(
-        params?: ApplePayDomainListParams,
-        options?: RequestOptions
-      ): ApiListPromise<Stripe.ApplePayDomain>;
-      list(options?: RequestOptions): ApiListPromise<Stripe.ApplePayDomain>;
+  /**
+   * List apple pay domains.
+   */
+  list(
+    params?: ApplePayDomainListParams,
+    options?: RequestOptions
+  ): ApiListPromise<Stripe.ApplePayDomain>;
+  list(options?: RequestOptions): ApiListPromise<Stripe.ApplePayDomain>;
 
-      /**
-       * Delete an apple pay domain.
-       */
-      del(
-        id: string,
-        params?: ApplePayDomainDeleteParams,
-        options?: RequestOptions
-      ): Promise<Stripe.Response<Stripe.DeletedApplePayDomain>>;
-      del(
-        id: string,
-        options?: RequestOptions
-      ): Promise<Stripe.Response<Stripe.DeletedApplePayDomain>>;
-    }
-  }
+  /**
+   * Delete an apple pay domain.
+   */
+  del(
+    id: string,
+    params?: ApplePayDomainDeleteParams,
+    options?: RequestOptions
+  ): Promise<Stripe.Response<Stripe.DeletedApplePayDomain>>;
+  del(
+    id: string,
+    options?: RequestOptions
+  ): Promise<Stripe.Response<Stripe.DeletedApplePayDomain>>;
 }
