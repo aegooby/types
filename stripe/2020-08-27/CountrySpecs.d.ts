7e6334f7be3a42cbd287f02bd3b2c8e7ab6179cf
aegooby <38363513+aegooby@users.noreply.github.com>
2021-03-25 21:35:23 +0300

    Update CountrySpecs.d.ts

--- stripe/2020-08-27/CountrySpecs.d.ts
+++ stripe/2020-08-27/CountrySpecs.d.ts
@@ -1,121 +1,117 @@
 // File generated from our OpenAPI spec
 
-declare module 'stripe' {
-  namespace Stripe {
-    /**
-     * The CountrySpec object.
-     */
-    interface CountrySpec {
+/**
+ * The CountrySpec object.
+ */
+interface CountrySpec {
+  /**
+   * Unique identifier for the object. Represented as the ISO country code for this country.
+   */
+  id: string;
+
+  /**
+   * String representing the object's type. Objects of the same type share the same value.
+   */
+  object: 'country_spec';
+
+  /**
+   * The default currency for this country. This applies to both payment methods and bank accounts.
+   */
+  default_currency: string;
+
+  /**
+   * Currencies that can be accepted in the specific country (for transfers).
+   */
+  supported_bank_account_currencies: {
+    [key: string]: Array<string>;
+  };
+
+  /**
+   * Currencies that can be accepted in the specified country (for payments).
+   */
+  supported_payment_currencies: Array<string>;
+
+  /**
+   * Payment methods available in the specified country. You may need to enable some payment methods (e.g., [ACH](https://stripe.com/docs/ach)) on your account before they appear in this list. The `stripe` payment method refers to [charging through your platform](https://stripe.com/docs/connect/destination-charges).
+   */
+  supported_payment_methods: Array<string>;
+
+  /**
+   * Countries that can accept transfers from the specified country.
+   */
+  supported_transfer_countries: Array<string>;
+
+  verification_fields: CountrySpec.VerificationFields;
+}
+
+namespace CountrySpec {
+  interface VerificationFields {
+    company: VerificationFields.Company;
+
+    individual: VerificationFields.Individual;
+  }
+
+  namespace VerificationFields {
+    interface Company {
       /**
-       * Unique identifier for the object. Represented as the ISO country code for this country.
+       * Additional fields which are only required for some users.
        */
-      id: string;
+      additional: Array<string>;
 
       /**
-       * String representing the object's type. Objects of the same type share the same value.
+       * Fields which every account must eventually provide.
        */
-      object: 'country_spec';
-
-      /**
-       * The default currency for this country. This applies to both payment methods and bank accounts.
-       */
-      default_currency: string;
-
-      /**
-       * Currencies that can be accepted in the specific country (for transfers).
-       */
-      supported_bank_account_currencies: {
-        [key: string]: Array<string>;
-      };
-
-      /**
-       * Currencies that can be accepted in the specified country (for payments).
-       */
-      supported_payment_currencies: Array<string>;
-
-      /**
-       * Payment methods available in the specified country. You may need to enable some payment methods (e.g., [ACH](https://stripe.com/docs/ach)) on your account before they appear in this list. The `stripe` payment method refers to [charging through your platform](https://stripe.com/docs/connect/destination-charges).
-       */
-      supported_payment_methods: Array<string>;
-
-      /**
-       * Countries that can accept transfers from the specified country.
-       */
-      supported_transfer_countries: Array<string>;
-
-      verification_fields: CountrySpec.VerificationFields;
+      minimum: Array<string>;
     }
 
-    namespace CountrySpec {
-      interface VerificationFields {
-        company: VerificationFields.Company;
-
-        individual: VerificationFields.Individual;
-      }
-
-      namespace VerificationFields {
-        interface Company {
-          /**
-           * Additional fields which are only required for some users.
-           */
-          additional: Array<string>;
-
-          /**
-           * Fields which every account must eventually provide.
-           */
-          minimum: Array<string>;
-        }
-
-        interface Individual {
-          /**
-           * Additional fields which are only required for some users.
-           */
-          additional: Array<string>;
-
-          /**
-           * Fields which every account must eventually provide.
-           */
-          minimum: Array<string>;
-        }
-      }
-    }
-
-    interface CountrySpecRetrieveParams {
+    interface Individual {
       /**
-       * Specifies which fields in the response should be expanded.
+       * Additional fields which are only required for some users.
        */
-      expand?: Array<string>;
-    }
-
-    interface CountrySpecListParams extends PaginationParams {
-      /**
-       * Specifies which fields in the response should be expanded.
-       */
-      expand?: Array<string>;
-    }
-
-    class CountrySpecsResource {
-      /**
-       * Returns a Country Spec for a given Country code.
-       */
-      retrieve(
-        id: string,
-        params?: CountrySpecRetrieveParams,
-        options?: RequestOptions
-      ): Promise<Stripe.Response<Stripe.CountrySpec>>;
-      retrieve(
-        id: string,
-        options?: RequestOptions
-      ): Promise<Stripe.Response<Stripe.CountrySpec>>;
+      additional: Array<string>;
 
       /**
-       * Lists all Country Spec objects available in the API.
+       * Fields which every account must eventually provide.
        */
-      list(
-        params?: CountrySpecListParams,
-        options?: RequestOptions
-      ): ApiListPromise<Stripe.CountrySpec>;
-      list(options?: RequestOptions): ApiListPromise<Stripe.CountrySpec>;
+      minimum: Array<string>;
     }
   }
+}
+
+interface CountrySpecRetrieveParams {
+  /**
+   * Specifies which fields in the response should be expanded.
+   */
+  expand?: Array<string>;
+}
+
+interface CountrySpecListParams extends PaginationParams {
+  /**
+   * Specifies which fields in the response should be expanded.
+   */
+  expand?: Array<string>;
+}
+
+class CountrySpecsResource {
+  /**
+   * Returns a Country Spec for a given Country code.
+   */
+  retrieve(
+    id: string,
+    params?: CountrySpecRetrieveParams,
+    options?: RequestOptions
+  ): Promise<Stripe.Response<Stripe.CountrySpec>>;
+  retrieve(
+    id: string,
+    options?: RequestOptions
+  ): Promise<Stripe.Response<Stripe.CountrySpec>>;
+
+  /**
+   * Lists all Country Spec objects available in the API.
+   */
+  list(
+    params?: CountrySpecListParams,
+    options?: RequestOptions
+  ): ApiListPromise<Stripe.CountrySpec>;
+  list(options?: RequestOptions): ApiListPromise<Stripe.CountrySpec>;
 }
